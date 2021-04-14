55556321ae2a546272ff49d07078ddc05019d39c
hossein-13 <hmashr@gmail.com>
2021-04-14 23:09:21 +0430

    urls

--- views.py
+++ views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 def articles_list(request):
     maqalat = models.STATE.objects.all().order_by('date')
     args = {'maqaleh':maqalat}
     return render(request, 'articleslist.html', args)
+
+def articles_detail(request, slug):
+    return HttpResponse(slug)
